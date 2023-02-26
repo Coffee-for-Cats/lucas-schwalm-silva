df207221cc1fbc85722a4f6b2fcb1dd57189239c
Lucas S. Silva <57202598+Coffee-for-Cats@users.noreply.github.com>
2023-02-25 21:45:03 -0300

    and there we goes

--- index.js
+++ index.js
@@ -9,5 +9,9 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/static/main.html"))
 })
 
+app.get("/googleabae759de29b6157.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "/googleabae759de29b6157.html"))
+})
+
 app.listen(5000, () => console.log("Running on port 5000"))
 module.exports = app;
