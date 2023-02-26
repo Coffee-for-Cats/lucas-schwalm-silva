7a96be3299362f80b55d94d01e30c199186c2a1b
Lucas S. Silva <57202598+Coffee-for-Cats@users.noreply.github.com>
2023-02-26 13:38:51 -0300

    beautiful details

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/static/main.html"))
 })
 
+//Google Verification
 app.get("/googleabae759de29b6157.html", (req, res) => {
     res.sendFile(path.join(__dirname, "/googleabae759de29b6157.html"))
 })
