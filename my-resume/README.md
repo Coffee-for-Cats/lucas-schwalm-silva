c97d9a81a6b613de58483ee5df343b776073eb9c
Lucas S. Silva <57202598+Coffee-for-Cats@users.noreply.github.com>
2023-10-10 21:25:22 -0300

    initializing with vue.js

---
+++ my-resume/README.md
@@ -0,0 +1,7 @@
+# My Personal Website
+This is my persoal website and my resume.
+
+# Made With:
+- Vue.js
+- Tailwind.css
+- Vercel for deployment.
---
+++ my-resume/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Hello vercel!</h1>
+  <p>From vue.js</p>
+</template>
+
+<style scoped>
+
+</style>
